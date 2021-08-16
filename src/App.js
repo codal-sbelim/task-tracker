95412b036e27c8fd62ffc006788420fd212c3896
Shahin Belim <sbelim@codal.com>
2021-08-16 16:41:11 +0530

    CIP-506  Task list page completed https://github.com/bradtraversy/react-crash-2021

--- src/App.js
+++ src/App.js
@@ -1,23 +1,52 @@
-import logo from './logo.svg';
-import './App.css';
+import Header from './components/Header'
+import Tasks from './components/Tasks';
+import { useState } from 'react';
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+  const [tasks, setTasks] = useState([
+    {
+        id: 1,
+        text: 'Doctors Appointment',
+        day: 'Feb 5th at 2:30pm',
+        reminder: true
+    },
+    {
+        id: 2,
+        text: 'Parents meeting at School',
+        day: 'Feb 6th at 2:30pm',
+        reminder: true
+    },
+    {
+        id: 3,
+        text: 'Food Shopping',
+        day: 'Feb 7th at 2:30pm',
+        reminder: false
+    }
+])
+
+// Delete Task
+const deleteTask = (id) => {
+  console.log('delete', id);
+  setTasks(tasks.filter((task) => task.id !== id ))
+}
+
+// Toggle Reminder
+const toggleReminder = (id) => {
+console.log(id);
+setTasks(tasks.map((task)=>task.id === id ? {...task, reminder: !task.reminder} : task))
+}
+
+return (
+    <div className="container">
+      <Header title="Task Tracker"/>
+      { tasks.length > 0 ?
+        <Tasks
+        tasks={tasks}
+        onDelete={deleteTask}
+        onToggle={toggleReminder}
+        />
+        : 'No tasks to show'
+      }
     </div>
   );
 }
