9af75e0bd2b3556e5280fa99e5d31eaedad54b6a
Shahin Belim <sbelim@codal.com>
2021-08-16 17:32:30 +0530

    CIP-506  Add new task form completed

--- src/App.js
+++ src/App.js
@@ -1,8 +1,10 @@
 import Header from './components/Header'
 import Tasks from './components/Tasks';
+import AddTask from './components/AddTask';
 import { useState } from 'react';
 
 function App() {
+  const [showAddTask, setShowAddTask] = useState(false);
   const [tasks, setTasks] = useState([
     {
         id: 1,
@@ -24,6 +26,14 @@ function App() {
     }
 ])
 
+// Add Tasks
+const addTask = (task) => {
+  const id = Math.floor(Math.random()* 10000) + 1;
+  console.log(id);
+  const newTask = { id, ...task };
+  setTasks([...tasks, newTask])
+}
+
 // Delete Task
 const deleteTask = (id) => {
   console.log('delete', id);
@@ -38,7 +48,8 @@ setTasks(tasks.map((task)=>task.id === id ? {...task, reminder: !task.reminder} 
 
 return (
     <div className="container">
-      <Header title="Task Tracker"/>
+      <Header title="Task Tracker" onAdd={() => setShowAddTask(!showAddTask)} showAdd={showAddTask}/>
+      { showAddTask && <AddTask onAdd={addTask}/>}
       { tasks.length > 0 ?
         <Tasks
         tasks={tasks}
